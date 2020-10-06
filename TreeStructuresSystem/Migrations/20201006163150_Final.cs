acf78215daf8e99f7f3ad711c9dd350151484534
matKrajnik <mateusz.krajnik97@gmail.com>
2020-10-06 18:32:52 +0200

    Final

---
+++ TreeStructuresSystem/Migrations/20201006163150_Final.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TreeStructuresSystem.Migrations
+{
+    public partial class Final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Nodes",
+                maxLength: 60,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Nodes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60,
+                oldNullable: true);
+        }
+    }
+}
